4e439a22ed42c5fdab296897cb580fc6e7b22ace
mrfawcett <136362421+mrfawcett@users.noreply.github.com>
2023-06-12 16:19:27 -0700

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester {
+    public static void main (String[] args) {
+        System.out.println(Main.multiply(2));
+        System.out.println(Main.multiply(3));
+        System.out.println(Main.square(2));
+        System.out.println(Main.square(4));
+
+    }
+}
