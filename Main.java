8216ec443d4bce8ba6bbac4fcb08606830fd649c
mrfawcett <136362421+mrfawcett@users.noreply.github.com>
2023-06-12 12:20:47 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /*
+     * TODO: Complete this method to multiply the parameter by 2
+     */
+    public static int multiply (int num) {
+        return 0;
+    }
+
+    /*
+     * TODO: Complete this method to square the parameter
+     */
+    public static int square (int num) {
+        return 0;
+    }
+}
